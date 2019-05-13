d14383e73246adb54abdeb6295ffac8d29e028ea
Sudhir <sudhirahirwar38@gmail.com>
2019-05-13 18:50:05 +0530

    Added GPUImage3 Lib

---
+++ imageeditor/imageeditor/bridgeclasses/GPUImageAdapter.swift
@@ -0,0 +1,15 @@
+//
+//  GPUImageAdapter.swift
+//  imageeditor
+//
+//  Created by Sudhir on 13/05/19.
+//  Copyright © 2019 HCL Technology Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+import GPUImage
+class GPUImageAdapter: NSObject {
+    func doOpe() {
+//        let b = BrightnessAdjustment
+    }
+}
---
+++ imageeditor/imageeditor/imageeditorDemoBridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  imageeditorDemoBridging-Header.h
+//  imageeditor
+//
+//  Created by Sudhir on 13/05/19.
+//  Copyright © 2019 HCL Technology Pvt Ltd. All rights reserved.
+//
+
+#ifndef imageeditorDemoBridging_Header_h
+#define imageeditorDemoBridging_Header_h
+#import <GPUImage/GPUImage.h>
+
+#endif /* imageeditorDemoBridging_Header_h */
